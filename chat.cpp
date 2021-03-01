8b235a0abfe02c2392293648f6935d23a3ca2b95
blueagle-so <blu.eagle@mail.ru>
2021-03-01 10:28:15 +0500

    chat stdin

--- chat.cpp
+++ chat.cpp
@@ -50,15 +50,40 @@ class Client: public Communication{
     }
     int run()
     {
-    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {return 0;}
+      for (;;){
+       fd_set rfds;
+       struct timeval tv;
+       int retval;
+       // Watch stdin (fd 0) to see when it has input.
+        FD_ZERO(&rfds);
+       FD_SET(0, &rfds);
+       // Wait up to five seconds.
+      tv.tv_sec = 5;
+       tv.tv_usec = 0;
+       retval = select(1, &rfds, NULL, NULL, &tv);
+       // Don't rely on the value of tv now!
+       ///if (retval == -1)
+      //perror("select()");
+      //else if (retval)
+      //printf("Data is available now.\n");
+    if(FD_ISSET(0, &rfds)){
+     //else
+   // printf("No data within five seconds.\n");
+
+
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+      if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {return 0;}
     memset(buffer, 0, sizeof(buffer));
     strcpy(buffer, "Hello Server");
+    read(0, buffer, sizeof(buffer));
     write(sockfd, buffer, sizeof(buffer));
     printf("Message from server: ");
     read(sockfd, buffer, sizeof(buffer));
     puts(buffer);
-    //close(sockfd);
-    //exit(0);
+    close(sockfd);
+    }
+      }
+    exit(0);
 
 }
 };
