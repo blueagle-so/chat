b2728dba42e0eeafc522d838c750a4b707c6438b
blueagle-so <blu.eagle@mail.ru>
2021-03-03 12:53:47 +0500

    chat

--- chat.cpp
+++ chat.cpp
@@ -13,15 +13,24 @@
  #define MAXLINE 1024
  #include <string.h>
 //#include "chat.h"
+
+typedef struct {
+	int socket;
+	struct sockaddr_in addres;
+	char* message;
+	char buffer[MAXLINE];
+} Peer;
+
 class Communication{
     public:
     //int PORT= 5000;
     //    //int MAXLINE= 1024;
+    Peer peer;    
     char* message;
     int sockfd;
     struct sockaddr_in servaddr,cliaddr;
     virtual void run(){}
-
+    char buffer[MAXLINE];
 
 };
 
@@ -41,21 +50,43 @@ class Client: public Communication{
     }
     void run()
     {
-    char buffer[MAXLINE];
     int n, len;
+  fd_set rset;
+for(;;){
+          sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    connect(sockfd, (struct sockaddr*)&servaddr,sizeof(servaddr)); //{
+	FD_ZERO(&rset);	
+     FD_SET(0, &rset);
+     FD_SET(sockfd, &rset); 
+       select(10, &rset, NULL, NULL, NULL);
+ if (FD_ISSET(0, &rset)){
+    //   connect(sockfd, (struct sockaddr*)&servaddr,sizeof(servaddr)); //{
 
-    if (connect(sockfd, (struct sockaddr*)&servaddr,
-    sizeof(servaddr)) < 0) {
-    printf("\n Error : Connect Failed \n");
-    }
-    memset(buffer, 0, sizeof(buffer));
-    strcpy(buffer, "Hello Server");
+	    read(0,buffer,sizeof(buffer));
+       //printf("\n Error : Connect Failed \n");
+    //}
+    //memset(buffer, 0, sizeof(buffer));
+    //strcpy(buffer, "Hello Server");
     write(sockfd, buffer, sizeof(buffer));
-    printf("Message from server: ");
-    read(sockfd, buffer, sizeof(buffer));
-    puts(buffer);
-    close(sockfd);
+    //printf("Message from server: ");
+    //read(sockfd, buffer, sizeof(buffer));
+    //puts(buffer);
+ //   close(sockfd);
+    
+ }
+   //        sockfd = socket(AF_INET, SOCK_STREAM, 0);   
+ //FD_SET(0, &rset);
+   /// select(10, &rset, NULL, NULL, NULL);
+ if (FD_ISSET(sockfd, &rset)){
+     read(sockfd, buffer, sizeof(buffer)); 
+	puts(buffer);
+ 
+ }
+ }
+
     }
+    
+
 };
 
 
@@ -91,26 +122,28 @@ bzero(&servaddr, sizeof(servaddr));
   socklen_t len;
   const int on = 1;
   void sig_chld(int);
-
-  // binding server addr structure to listenfd
+//for(;;){
+  //   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	// binding server addr structure to listenfd
    bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
    listen(sockfd, 10);
 
    /* create UDP socket */
-   udpfd = socket(AF_INET, SOCK_DGRAM, 0);
+   //udpfd = socket(AF_INET, SOCK_DGRAM, 0);
    // binding server addr structure to udp sockfd
-   bind(udpfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+   //bind(udpfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
+   for(;;){
   // clear the descriptor set
   FD_ZERO(&rset);
 
   // get maxfd
   maxfdp1 = max(sockfd, udpfd) + 1;
-  for (;;) {
+  //for (;;) {
 
   // set listenfd and udpfd in readset
   FD_SET(sockfd, &rset);
-  FD_SET(udpfd, &rset);
+  FD_SET(0, &rset);
 
    // select the ready descriptor
    nready = select(maxfdp1, &rset, NULL, NULL, NULL);
@@ -119,31 +152,39 @@ bzero(&servaddr, sizeof(servaddr));
    // it by accepting the connection
    if (FD_ISSET(sockfd, &rset)) {
    len = sizeof(cliaddr);
-   connfd = accept(sockfd, (struct sockaddr*)&cliaddr, &len);
+   connfd = accept(sockfd, NULL, NULL);
    if ((childpid = fork()) == 0) {
-   close(sockfd);
+   //close(sockfd);
    bzero(buffer, sizeof(buffer));
    printf("Message From TCP client: ");
    read(connfd, buffer, sizeof(buffer));
    puts(buffer);
-   write(connfd, (const char*)message, sizeof(buffer));
+   write(connfd, buffer, sizeof(buffer));
    close(connfd);
    exit(0);
    }
    close(connfd);
    }
    // if udp socket is readable receive the message.
-   if (FD_ISSET(udpfd, &rset)) {
-   len = sizeof(cliaddr);
-   bzero(buffer, sizeof(buffer));
-   printf("\nMessage from UDP client: ");
-   n = recvfrom(udpfd, buffer, sizeof(buffer), 0,
-   (struct sockaddr*)&cliaddr, &len);
-   puts(buffer);
-   sendto(udpfd, (const char*)message, sizeof(buffer), 0,
-   (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+   if (FD_ISSET(0, &rset)) {
+   connfd = accept(sockfd, NULL, NULL);
+     if ((childpid = fork()) == 0) {
+	   //    close(sockfd);
+	         bzero(buffer, sizeof(buffer));
+		   //printf("Message From TCP client: ");
+		     read(0, buffer, sizeof(buffer));
+		       puts(buffer);
+		         write(connfd, (const char*)buffer, sizeof(buffer));
+			   close(connfd);
+			     exit(0);
+			       }
+       close(connfd);
+         } 
+   
+
+
+ 	  
    }
-  }
  }
 };
 
@@ -174,7 +215,7 @@ Communication *comm;
   sizeof(servaddr)) < 0) {
   comm=new Server();
   }else {comm=new Client();}
-
+close (sockfd);
     comm->run();
 
 
