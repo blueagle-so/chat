4dfa46e1b1aa2408b898d1999d764aefbe7fa003
blueagle-so <blu.eagle@mail.ru>
2021-03-12 16:50:26 +0500

    modifi server

--- chat.cpp
+++ chat.cpp
@@ -47,7 +47,7 @@ class Client: public Communication{
     }
     void run()
     {
-    int n, len;
+    //int n, len;
 
     if (connect(sockfd, (struct sockaddr*)&servaddr,
     sizeof(servaddr)) < 0) {
@@ -115,57 +115,11 @@ class Server : public Communication{
     /* Variable and structure definitions. */
     int sd, sd2, rc, length = sizeof(int);
     int totalcnt = 0, on = 1;
-    //char temp;
-      //char buffer[BufferLength];
-      // struct sockaddr_in serveraddr;
-      //struct sockaddr_in their_addr;
-      //fd_set read_fd;
-      //struct timeval timeout;
-       /*timeout.tv_sec = 15;*/
-       /*timeout.tv_usec = 0;*/	
 	Server()
 	{
-	// timeout.tv_sec = 15;
-        // timeout.tv_usec = 0;    
- 	/* Get a socket descriptor */
-	//if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	//{
-	//perror("Server-socket() error");
-	/* Just exit */
-	//exit (-1);
-	//}
-	//else
-	printf("Server-socket() is OK\n");
-	/* Allow socket descriptor to be reusable */
-	//if((rc = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on))) < 0)
-	//{
-	//perror("Server-setsockopt() error");
-	//close(sockfd);
-	//exit (-1);
-	//}
-	//else
-	//printf("Server-setsockopt() is OK\n");
-	/* bind to an address */
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	/*printf("Using %s, listening at %d\n", inet_ntoa(serveraddr.sin_addr), SERVPORT);*/
-	if((rc = bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) < 0)
-	{
-	perror("Server-bind() error");
-	/* Close the socket descriptor */
-	/* and just exit */
-	exit(-1);
-	}
-	else
-	printf("Server-bind() is OK\n");
-	/* Up to 10 clients can be queued */
-	if((rc = listen(sockfd, 10)) < 0)
-	{
-        perror("Server-listen() error");
-        close(sockfd);
-        exit (-1);
-    	}
-	else
-	printf("Server-Ready for client connection...\n");
+	bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	listen(sockfd, 10);
 	}
 	void run(){
 
@@ -173,98 +127,20 @@ class Server : public Communication{
 	int sin_size = sizeof(struct sockaddr_in);
 
 
-sd2 = accept(sockfd, (struct sockaddr *)&cliaddr, &sin_size);    	
-if((sd2 = accept(sockfd, (struct sockaddr *)&cliaddr, &sin_size)) < 0)
-	{
-	perror("Server-accept() error");
-	close(sockfd);
-	exit (-1);
-	}
-	else
-	printf("Server-accept() is OK\n");
-	/*client IP*/
-	printf("Server-new socket, sd2 is OK...\n");
-	/*printf("Got connection from the f***ing client: %s\n", inet_ntoa(their_addr.sin_addr));*/
-									
-	/* Wait for up to 15 seconds on */
-	/* select() for data to be read. */
+//sd2 = accept(sockfd, (struct sockaddr *)&cliaddr, &sin_size);    	
+	sd2 = accept(sockfd, NULL, NULL);
+	sd2 = accept(sockfd, NULL, NULL);           
 	FD_ZERO(&read_fd);
         FD_ZERO(&write_fd);
 	//FD_SET(0, &read_fd);
 	FD_SET(sd2, &read_fd);
 	FD_SET(sd2, &write_fd);
-	rc = select(sd2+1, &read_fd, &write_fd, NULL, NULL);
-	if((rc == 1) && (FD_ISSET(sd2, &write_fd)))
-	{
-	/* Read data from the client. */
-	totalcnt = 0;
-	while(totalcnt < BufferLength)
-	{
-	/* read() from client */
-	rc = read(sd2, &buffer[totalcnt], (BufferLength - totalcnt));
-	//rc = write(sd2,"hello world",15);
-	if(rc < 0)
-	{
-	perror("Server-read() error");
-	close(sockfd);
-	close(sd2);
-	exit (-1);
-	}
-	else if (rc == 0)
-	{
-	printf("Client program has issued a close()\n");
-	close(sockfd);
-	close(sd2);
-	exit(-1);
-	}
-	else
-	{
-	totalcnt += rc;
-	printf("Server-read() is  OK\n");
-	}
-	}
-	}
-	else if (rc < 0)
-	{
-	perror("Server-select() error");
-	close(sockfd);
-	close(sd2);
-	exit(-1);
-	}
-	/* rc == 0 */
-	else
-	{
-	printf("Server-select() timed out.\n");
-	close(sockfd);
-	close(sd2);
-	exit(-1);
-	}
-	/* Shows the data */
+	select(sd2+1, &read_fd, &write_fd, NULL, NULL);
+	if(FD_ISSET(sd2, &write_fd))
+	read(sd2, &buffer[totalcnt], (BufferLength - totalcnt));
 	printf("Received data from the f***ing client: %s\n", buffer);
-	/* write() some bytes of string, */
-	/* back to the client. */
 	printf("Server-Echoing back to client...\n");
-	rc = write(sd2, buffer, totalcnt);
-	if(rc != totalcnt)
-	{
-	perror("Server-write() error");
-	/* Get the error number. */
-	//rc = getsockopt(sd2, SOL_SOCKET, SO_ERROR, &temp, &length);
-	if(rc == 0)
-	{
-	/* Print out the asynchronously */
-	/* received error. */
-	//errno = temp;
-	perror("SO_ERROR was: ");
-	}
-	else
-	printf("Server-write() is OK\n");
-	close(sockfd);
-	close(sd2);
-	exit(-1);
-	}
-	/* Close the connection to the client and */
-	/* close the server listening socket. */
+	write(sd2, buffer, totalcnt);
 	close(sd2);
 	close(sockfd);
 	/*exit(0);*/
